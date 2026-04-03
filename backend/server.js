407e0ed5e8f68d0fb2ed5aa75b872b32507d9e6d
ajayjoshii <jajay7941@gmail.com>
2026-04-03 21:26:19 +0545

    new

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: "https://newdemo-1111.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
